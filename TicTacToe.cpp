cfb17ed281a6f4dd07c5e130415fb9e661964302
tonybishnoi <53338071+tonybishnoi@users.noreply.github.com>
2019-10-02 00:21:06 +0530

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,30 +1,102 @@
 #include<iostream>
+#include<stdlib.h>
+#include<fstream>
 using namespace std;
-string board[3][3];
+char a[4][4] = { "   ", "   ", "   " };
 void display();
-void start();
+char checkwin();
+int playturn(char);
 int main()
 {
+	int i;
+	char turn = 'X', wn;
+	for (i = 0; i < 9; i++)
+	{
+		display();
+		cout << "\nPlayer " << turn << " turn: ";
+		if (playturn(turn) != 0)
+		{
+			turn == 'X' ? turn = 'O' : turn = 'X';
+			system("CLS");
+			wn = checkwin();
+			if (wn != ' ')
+				break;
+			wn = 'N';
+		}
+		else
+		{
+			cout << "\nIllegal move. Try again!\n";
+			--i;
+		}
+	}
 
+	system("cls");
+	if (wn == 'N')
+		cout << "It's a tie. Nobody won!\n\n";
+	else
+		cout << "PLAYER " << wn << " WON !!!\n\n";
+	display();
+	return 0;
 }
 void display()
 {
-    for(int i=0;i<3;i++)
-    {
-        for(int j=0;j<3;j++)
-        {
-            cout<<board[i][j]<<"|";
-        }
-        cout<<endl;
-    }
+	int i;
+	for (i = 0; i < 3; i++)
+	{
+		cout << a[i][0] << "|" << a[i][1] << "|" << a[i][2] << "\n";
+	}
 }
-void start()
+char checkwin()
 {
-    for(int i=0;i<3;i++)
-    {
-        for(int j=0;j<3;j++)
-        {
-            board[i][j]=" ";
-        }
-    }
+	int i = 0;
+	for (i = 0; i < 3; i++)
+	{
+		if (a[i][0] == a[i][1] && a[i][1] == a[i][2])
+			return a[i][0];
+		else if (a[0][i] == a[1][i] && a[1][i] == a[2][i])
+			return a[0][i];
+	}
+	if (a[0][0] == a[1][1] && a[1][1] == a[2][2])
+		return a[0][0];
+	else if (a[0][2] == a[1][1] && a[1][1] == a[2][0])
+		return a[0][2];
+	return ' ';
+}
+int playturn(char ch)
+{
+	int inp, legal = 1;
+	cin >> inp;
+	switch (inp)
+	{
+	case 1:
+		a[0][0] == ' ' ? a[0][0] = ch : legal = 0;
+		break;
+	case 2:
+		a[0][1] == ' ' ? a[0][1] = ch : legal = 0;
+		break;
+	case 3:
+		a[0][2] == ' ' ? a[0][2] = ch : legal = 0;
+		break;
+	case 4:
+		a[1][0] == ' ' ? a[1][0] = ch : legal = 0;
+		break;
+	case 5:
+		a[1][1] == ' ' ? a[1][1] = ch : legal = 0;
+		break;
+	case 6:
+		a[1][2] == ' ' ? a[1][2] = ch : legal = 0;
+		break;
+	case 7:
+		a[2][0] == ' ' ? a[2][0] = ch : legal = 0;
+		break;
+	case 8:
+		a[2][1] == ' ' ? a[2][1] = ch : legal = 0;
+		break;
+	case 9:
+		a[2][2] == ' ' ? a[2][2] = ch : legal = 0;
+		break;
+	default:
+		legal = 0;
+	}
+	return legal;
 }
